102eed2eac5613dc90290f631d60666f1dbef2e7
Gabriel Muñoz Barahona <gm.barahona911@gmail.com>
2023-12-04 23:27:39 -0300

    fix: Remove Unused imports

--- src/main/java/org/soyunmate/mail/sender/controller/MailController.java
+++ src/main/java/org/soyunmate/mail/sender/controller/MailController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
--- src/main/java/org/soyunmate/mail/sender/domain/EmailFileDTO.java
+++ src/main/java/org/soyunmate/mail/sender/domain/EmailFileDTO.java
@@ -3,7 +3,6 @@ package org.soyunmate.mail.sender.domain;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 
 @Getter
 @Setter
